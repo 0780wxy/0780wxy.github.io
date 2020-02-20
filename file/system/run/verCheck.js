ad16b85db6dc35a7a4bfea04921c7825d94b9dae
0780wxy <31151586+0780wxy@users.noreply.github.com>
2020-02-20 12:58:29 +0800

    Update verCheck.js

--- file/system/run/verCheck.js
+++ file/system/run/verCheck.js
@@ -1,14 +1,17 @@
-$.ajax({
-  type : "get",
-  url : "version.txt",
-  dataType : "text",
-  cache : false,
-  success : function(text){
-    var ver = text;
-    ver = ver.slice(0,ver.indexOf("\n"));
-    if(ver > localStorage.getItem("osVer")){
-      verCheck = true;
-      eval(file.read("/apps/updata/index.js"));
+verCheck = function(){
+  $.ajax({
+    type : "get",
+    url : "version.txt",
+    dataType : "text",
+    cache : false,
+    success : function(text){
+      var ver = text;
+      ver = ver.slice(0,ver.indexOf("\n"));
+      if(ver > localStorage.getItem("osVer")){
+        verCheck = true;
+        eval(file.read("/apps/updata/index.js"));
+      }
     }
-  }
-});
+  });
+};
+iniFun.push("verCheck");
